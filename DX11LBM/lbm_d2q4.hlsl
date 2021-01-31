250a355f209a6db3377533dda5ec689fe841773e
brainburster <brainburster@outlook.com>
2021-01-31 12:03:47 +0800

    准备实际写lbm shader算法

--- DX11LBM/lbm_d2q4.hlsl
+++ DX11LBM/lbm_d2q4.hlsl
@@ -3,9 +3,20 @@ RWTexture2D<float4> outTex : register(u0);
 RWTexture2D<float4> f_in : register(u1);
 RWTexture2D<float4> f_out : register(u2);
 
+void collisionStep(uint2 index)
+{
+}
+
+void streamingStep(uint2 index)
+{
+}
+
 [numthreads(1, 1, 1)]
 void main(uint3 DTid : SV_DispatchThreadID)
 {
-	float4 color = float4(0.5, 0.5, 0.5, 1);
-	outTex[DTid.xy] = clamp(color - inTex[DTid.xy], float4(0, 0, 0, 0), float4(1, 1, 1, 1));
+	collisionStep(DTid.xy);
+	GroupMemoryBarrierWithGroupSync();
+	streamingStep(DTid.xy);
+	GroupMemoryBarrierWithGroupSync();
+	outTex[DTid.xy] = clamp(f_in[DTid.xy] + inTex[DTid.xy], float4(0, 0, 0, 0), float4(1, 1, 1, 1));
 }
