17d358a40053771c3b542b5074ba0f0a15722786
brainburster <brainburster@outlook.com>
2021-10-12 23:07:12 +0800

    修改显示

--- DX11LBM/init.hlsl
+++ DX11LBM/init.hlsl
@@ -5,13 +5,14 @@ void main( uint3 DTid : SV_DispatchThreadID )
 {
     const uint2 index = DTid.xy;
 
-    f[uint3(index, 0)] = 0.16f;
-    f[uint3(index, 1)] = 0.17f;
-    f[uint3(index, 2)] = 0.16f;
+    f[uint3(index, 0)] = 0.2f;
+    f[uint3(index, 1)] = 0.2f;
+    f[uint3(index, 2)] = 0.2f;
     f[uint3(index, 3)] = 0.2f;
     f[uint3(index, 4)] = 0.2f;
     f[uint3(index, 5)] = 0.2f;
     f[uint3(index, 6)] = 0.2f;
     f[uint3(index, 7)] = 0.2f;
     f[uint3(index, 8)] = 0.2f;
+
 }
--- DX11LBM/visualization.hlsl
+++ DX11LBM/visualization.hlsl
@@ -15,21 +15,23 @@ void main( uint3 DTid : SV_DispatchThreadID )
         return;
     }
     
-    float rho[4] = { 0, 0, 0, 0 };
-    float2 u[4] = { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } };
+    float rho[5] = { 0, 0, 0, 0, 0 };
+    float2 u[5] = { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } };
     
-    [unroll(9)]
     for (uint i = 0; i < 9; i++)
     {
         rho[0] += f_in[uint3(index + int2(1, 0), i)];
         rho[1] += f_in[uint3(index + int2(0, 1), i)];
         rho[2] += f_in[uint3(index + int2(-1, 0), i)];
         rho[3] += f_in[uint3(index + int2(0, -1), i)];
+        rho[4] += f_in[uint3(index,i)];
+        
         
         u[0] += f_in[uint3(index + int2(1, 0), i)] * v[i];
         u[1] += f_in[uint3(index + int2(0, 1), i)] * v[i];
         u[2] += f_in[uint3(index + int2(-1, 0), i)] * v[i];
         u[3] += f_in[uint3(index + int2(0, -1), i)] * v[i];
+        u[4] += f_in[uint3(index, i)] * v[i];
     }
     
     u[0] /= rho[0];
@@ -40,8 +42,10 @@ void main( uint3 DTid : SV_DispatchThreadID )
     float dv = (u[1].x - u[3].x) * 0.5f;
     float w = du - dv;
     
-    float3 a = { 0.8f - abs(w) * 25, 0.8f - du * 20, 0.8f - dv * 20 };
+    //float3 a = { 0.8f - abs(w) * 25, 0.8f - du * 20, 0.8f - dv * 20 };
     //float3 a = 0.8f - abs(w) * 20;
+    float2 v = u[4] / rho[4];
+    float3 a = length(v)*3.0;
     
     outTex[index] = lerp(saturate(float4(a, 1.f)), float4(0.18f, 0.18f, 0.18f, 1.f), saturate((inTex[index]).r * 2));
 }
