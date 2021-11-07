dcd7da47500bdd3fd733eff903e115f7c0894fd9
brainburster <brainburster@outlook.com>
2021-11-07 11:20:34 +0800

    截一下图

--- Multicomponent/ps.hlsl
+++ Multicomponent/ps.hlsl
@@ -12,7 +12,7 @@ float4 main(VsOut vs_out) : SV_TARGET
 {
     float4 data = srv_display.Sample(srv_display_sampler, vs_out.uv);
     const float4 back_color = float4(vs_out.uv, 0.f, 1.0f);
-    const float4 wall_color = float4(0.1f, 0.1f, 0.1f, 1.0f);
+    const float4 wall_color = float4(0.2f, 0.2f, 0.2f, 1.0f);
     const float4 color = float4(float3(.8f,.8f,.8f)-data.xyz, 1.0f);
     //return color3;
     if (data.w < 0.0f)
